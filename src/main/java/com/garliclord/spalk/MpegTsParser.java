ed89326e4d97957a50191526e277121c512ccfe1
Sulbin Kim <sustel@gmail.com>
2024-06-16 18:58:44 +1200

    add mpegts parsing

--- src/main/java/com/garliclord/spalk/MpegTsParser.java
+++ src/main/java/com/garliclord/spalk/MpegTsParser.java
@@ -1,10 +1,82 @@
 package com.garliclord.spalk;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MpegTsParser {
+    static final byte SYNC_BYTE = 0x47;
+    static final int PACKET_LENGTH = 188;
+    int errorPacketIndex = -1;
+    int errorByteOffset = -1;
 
     public Result parse(InputStream input) {
-        throw new UnsupportedOperationException();
+        try (BufferedInputStream bin = new BufferedInputStream(input)) {
+
+            int temp;
+            do {
+                temp = bin.read();
+//                System.out.println(temp);
+                if (temp == SYNC_BYTE) {
+                    bin.mark(PACKET_LENGTH);
+                    bin.skip(PACKET_LENGTH - 1);
+                    temp = bin.read();
+                    if (temp == SYNC_BYTE) {
+                        //then break out
+                        bin.reset();
+                    }
+
+                    // mark this position then reset back to it if another syncbyte found in 188 bytes
+                    //now we know this is a true start of a packet
+                    //then parse out the pid from 2nd and 3rd bytes, skip to 188th byte then repeat
+                }
+            } while ((temp != -1 && temp != SYNC_BYTE));
+
+
+            // found start of first complete packet
+            int start = temp;
+            byte[] pidContainer = new byte[2];
+            List<Integer> pids = new ArrayList<>();
+            int packetCount = 0;
+
+            //foreach packet get pid
+            //assume we start with syncbyte
+            if (start == SYNC_BYTE) {
+                while (temp != -1) {
+                    temp = bin.read(pidContainer);
+
+                        pids.add(twoBytesToPid(pidContainer));
+                        bin.skip(PACKET_LENGTH - 3);
+                        packetCount ++;
+                        if (bin.read() != SYNC_BYTE){
+                            return new Result(false, List.of(), packetCount, errorByteOffset);
+                            //TODO need to keep count of byteoffset
+                        };
+                }
+            } else {
+                System.out.println("input doesn't have a syncbyte??");
+            }
+
+            //output result
+            return new Result(true, pids, -1, -1);
+
+        } catch (IOException e) {
+            return new Result(false, List.of(), errorPacketIndex, errorByteOffset);
+        }
+    }
+
+    private int twoBytesToPid(byte[] twoBytes) {
+        ByteBuffer bb = ByteBuffer.allocate(2);
+        bb.put(twoBytes[0]);
+        bb.put(twoBytes[1]);
+        short shortVal = bb.getShort(0);
+
+        //Get last 5 bits of the first byte and all 8 bits of the second
+        int mask = Integer.parseUnsignedInt("000111111111111", 2);
+        return shortVal & mask;
     }
 }
--- src/test/java/com/garliclord/spalk/MpegTsParserTest.java
+++ src/test/java/com/garliclord/spalk/MpegTsParserTest.java
@@ -2,11 +2,11 @@ package com.garliclord.spalk;
 
 import org.junit.jupiter.api.Test;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.List;
+import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class MpegTsParserTest {
 
@@ -18,6 +18,7 @@ class MpegTsParserTest {
 
         Result result = parser.parse(fileInput);
 
+        assertTrue(result.isSuccessful());
         assertEquals(List.of(0x0, 0x11, 0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x1fff), result.pids());
     }
 
@@ -29,7 +30,21 @@ class MpegTsParserTest {
 
         Result result = parser.parse(fileInput);
 
+        assertFalse(result.isSuccessful());
         assertEquals(20535, result.errorPacketIndex());
         assertEquals(3860580, result.errorByteOffset());
+    }
+
+    @Test
+    void parseTest() throws FileNotFoundException {
+
+        MpegTsParser parser = new MpegTsParser();
+
+        byte[] initialArray = { 0, 1, 2 };
+        InputStream targetStream = new ByteArrayInputStream(initialArray);
+
+        Result result = parser.parse(targetStream);
+
+        assertEquals(List.of(0x0, 0x11, 0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x1fff), result.pids());
     }
 }
